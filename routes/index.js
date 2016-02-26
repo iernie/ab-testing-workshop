bdcdc37d686df6f99cdc25f3d79a11c7d2b4f593
ernie <ernie@epicbytes.net>
2016-02-26 19:03:47 +0100

    wip3

--- routes/index.js
+++ routes/index.js
@@ -2,27 +2,18 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  var random = Math.random();
-  if(random <= 0.5) {
-    res.redirect('/one');
-  } else {
-    res.redirect('/two');
-  }
-});
-
-router.get('/one', function(req, res, next) {
   res.render('one');
 });
 
-router.post('/one', function(req, res, next) {
+router.post('/', function(req, res, next) {
   res.render('thanks', {variant: "one"});
 });
 
-router.get('/two', function(req, res, next) {
+router.get('/variant', function(req, res, next) {
   res.render('two');
 });
 
-router.post('/two', function(req, res, next) {
+router.post('/variant', function(req, res, next) {
   res.render('thanks', {variant: "two"});
 });
 
